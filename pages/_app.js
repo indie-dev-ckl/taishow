f356648b25399feb0126acf51a65a9ec64dfb31d
lok <t-klcheng@shkp.com>
2022-10-26 12:43:18 +0800

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,13 @@ import '../styles/globals.scss'
 import 'tailwindcss/tailwind.css'
 import React, {useEffect, useState} from 'react';
 import {Layout} from '../components';
+import { Analytics } from '@vercel/analytics/react';
+
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   
   )

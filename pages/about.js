5afddf827efbbd98a0d909b6c54c1bd8b164f551
lok <t-klcheng@shkp.com>
2022-11-10 16:46:16 +0800

    fix minor issue

--- pages/about.js
+++ pages/about.js
@@ -10,7 +10,16 @@ export default function AudioBufferOptions(){
             <div className="flex flex-col">
                 <h4 className="text-lg font-semibold text-center md:text-left">Lok</h4>
                 <p className="dark:text-gray-400 tracking-widest">
-                    一個鍾情於音樂嘅人
+                    一個鍾情於音樂嘅人 / IT狗 / Indie仔
+                </p>
+                <p className='dark:text-gray-400 tracking-widest'>
+                    除咗係呢個網嘅製作者,仲係一個keep住執譜嘅結他佬
+                </p>
+                <p className='dark:text-gray-400 tracking-widest'>
+                    My Instagram:
+                </p>
+                <p className='dark:text-gray-400 tracking-widest'>
+                    My Youtube Channel:
                 </p>
 		    </div>
 	    </div>
@@ -23,10 +32,10 @@ export default function AudioBufferOptions(){
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <div className='mx:auto px-12 dark:bg-gray-900 dark:text-gray-100'>
-                <p class="mb-3  dark:text-gray-400 first-line:uppercase first-line:tracking-widest first-letter:text-7xl first-letter:font-bold first-letter:text-gray-900 dark:first-letter:text-gray-100 first-letter:mr-3 first-letter:float-left">
+                <p className="mb-3  dark:text-gray-400 first-line:uppercase first-line:tracking-widest first-letter:text-7xl first-letter:font-bold first-letter:text-gray-900 dark:first-letter:text-gray-100 first-letter:mr-3 first-letter:float-left">
                     睇騷是一個資源整合的網站,其唯一目標是提供一個平台令人快速查看近期有咩show睇
                 </p>
-                <p class=" dark:text-gray-400 first-line:tracking-widest">由於網站內的post都係由我自己手動copy落嚟的,所以可能會有所缺失,如果你係活動搞手,歡迎透過以下方法</p>
+                <p className=" dark:text-gray-400 first-line:tracking-widest">由於網站內的post都係由我自己手動copy落嚟的,所以可能會有所缺失,如果你係活動搞手,歡迎透過以下方法</p>
             </div>
         </div>
         </>
